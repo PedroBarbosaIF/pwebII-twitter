f72c337dd4d2f81d7e624578ca2fcb0e03840d50
Pedro Barbosa <pedro.barbosa40@aluno.ifce.edu.br>
2025-11-01 22:43:12 -0300

    Adicionar classe 'texto' para melhorar a formatação do texto nos comentários.

--- Mordecai_PWEBII/src/Components/PostPageComment/PostPageCardComment.jsx
+++ Mordecai_PWEBII/src/Components/PostPageComment/PostPageCardComment.jsx
@@ -1,11 +1,13 @@
+import './style.css'
+
 export default function PostPageCardComment(props) {
     return (
         <div
             className="post max-w-sm bg-white rounded-lg shadow-md p-6 flex items-start justify-between gap-2"
             id={props.htmlId ? "individual" : undefined}
         >
-            <div className="flex-1">
-                <div className="text-gray-600">{props.text}</div>
+            <div className=" flex-1">
+                <div className="text-gray-600 texto">{props.text}</div>
             </div>
             <button
                 className="ml-2 px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 transition"
